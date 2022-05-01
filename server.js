ddf48822ea46942d9381a885e634759562c6ed93
Andrew Noorishad <anoorishad@me.com>
2022-05-01 12:00:49 -0700

    updated server routes

--- server.js
+++ server.js
@@ -6,10 +6,30 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
+
+// Middleware for parsing JSON and urlencoded from data
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get
 
+
+// GET Route for homepage
+app.get('/', (req,res) => 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Get Route for notes page
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//Wildcard route to direct users back to homepage
+app.get('*', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
