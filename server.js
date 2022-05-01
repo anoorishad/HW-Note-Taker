a4aa650897099cf30cb3fb9642ea51eb2aaefb24
Andrew Noorishad <anoorishad@me.com>
2022-05-01 14:07:09 -0700

    added get route for api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
@@ -25,10 +26,21 @@ app.get('/notes', (req,res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//  GET ROUTE for retrieving all the notes
+express.Router('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json')
+});
+
+
 //Wildcard route to direct users back to homepage
 app.get('*', (req,res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+
+
+
+
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
